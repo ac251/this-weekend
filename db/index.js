99abdc5d1afe46a373250c6fcae0396ee972f8a3
Aaron Cohn <aaron.d.cohn@gmail.com>
2018-12-03 01:20:47 -0800

    More db queries

--- db/index.js
+++ db/index.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool();
 
 module.exports.checkRoomAuth = (uuid, roomId) => {
-  const queryStr = 'SELECT users_rooms.joined FROM users INNER JOIN users_rooms ON users.id = users_rooms.user WHERE users.uuid = $1';
-  return pool.query(queryStr, [uuid])
+  const queryStr = 'SELECT users_rooms.joined FROM users INNER JOIN users_rooms ON users.id = users_rooms.user WHERE users.uuid = $1 AND users_rooms.room = $2';
+  return pool.query(queryStr, [uuid, roomId])
     .then(({ rows }) => {
       if (rows.length === 0) {
         throw new Error('unauthorized');
@@ -30,3 +30,20 @@ module.exports.getRooms = (uuid) => {
   return pool.query(queryStr, [uuid])
     .then(({ rows }) => rows);
 };
+
+module.exports.createRoom = (uuid, roomName) => {
+  const findUserQuery = 'SELECT id as user FROM users WHERE uuid = $1';
+  const createRoom = 'INSERT INTO rooms (name) VALUES ($1) RETURNING id as room';
+  const linkUserToRoom = 'INSERT INTO users_rooms (user, room) VALUES ($1, $2)';
+  
+  return pool.query(findUserQuery, [uuid])
+    .then(({ rows }) => {
+      const { id } = rows[0];
+      return pool.query(createRoom, [roomName])
+        .then(({ rows }) => {
+          const { room } = rows[0];
+          return room;
+        })
+        .then(room => pool.query(linkUserToRoom, [id, room]));
+    });
+}
