57c2e5a1f00bc030d4f26b62c6f5b51c7117ee74
Aaron Cohn <aaron.d.cohn@gmail.com>
2018-12-03 02:28:34 -0800

    Sketched out invite panel

--- client/components/App.vue
+++ client/components/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <main>
+      <RoomsList
+        v-if="choosingRoom"
+        @changeRoom="changeRoom"
+        :rooms = "rooms"
+        :currentRoom = "currentRoom"
+        @back="choosingRoom = false"
+      />
+      <Invite
+        v-else-if="inviting"
+        @back="inviting = false"
+      />
+      <header v-else>
+        
+      </header>
       <Message
         v-for="(message, index) in messages"
         :key="index"
@@ -12,13 +26,19 @@
 </template>
 <script>
   import Message from './Message.vue';
+  import RoomsList from './RoomsList.vue';
   import messages from './sampleData.js';
   export default {
-    components: { Message },
+    components: { Message, RoomsList },
     data() {
       return {
         messages, //update when no longer using sample data
         user: '',
+        choosingRoom: false,
+        inviting: false,
+        rooms: [{id: 1, name: 'test'}], //get rid of this sample stuff
+        currentRoom: 'the best room', // this too
+
       }
     },
     created() {
@@ -27,7 +47,10 @@
     methods: {
       test(id) {
         console.log('ID IS', id);
-      }
+      },
+      changeRoom(roomId) {
+        // call api, get messages
+      } 
     }
 
   }  
---
+++ client/components/Invite.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>invite people to {{room}}</h1>
+    <form>
+      <input type="text" />
+      <button>
+        send invite
+      </button>
+    </form>
+    <div v-if="sent"
+      :class="success ? 'invite-success' : 'invite-failure'"
+    >
+      {{success ? 'invite sent' : 'we couldn\'t find anyone by that name'}}
+    </div>
+  </header>
+</template>
+<script>
+  module.exports = {
+    props: ['room'],
+    data() {
+      return {
+        sent: false,
+      }
+    },
+    submit() {
+      // submit user
+    }
+  }
+  
+</script>
