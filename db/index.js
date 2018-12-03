d821760351427e5a9e52b178d022ab480991dd9f
Aaron Cohn <aaron.d.cohn@gmail.com>
2018-12-03 11:30:00 -0800

    Added query to find user by name, small fixes on other queries

--- db/index.js
+++ db/index.js
@@ -4,6 +4,7 @@ const pool = new Pool();
 
 module.exports.checkRoomAuth = (uuid, roomId) => {
   const queryStr = 'SELECT users_rooms.joined FROM users INNER JOIN users_rooms ON users.id = users_rooms.user WHERE users.uuid = $1 AND users_rooms.room = $2';
+  
   return pool.query(queryStr, [uuid, roomId])
     .then(({ rows }) => {
       if (rows.length === 0) {
@@ -14,29 +15,32 @@ module.exports.checkRoomAuth = (uuid, roomId) => {
 };
 
 module.exports.getAllMessages = (roomId) => {
-  const queryStr = 'SELECT messages.*, users_name FROM messages INNER JOIN rooms ON messages.user = users.id WHERE messages.room = $1';
+  const queryStr = 'messages.body, messages.room, messages.posted, users.name AS user FROM messages INNER JOIN users ON messages.user = users.id WHERE messages.room = $1';
+  
   return pool.query(queryStr, [roomId])
     .then(({ rows }) => rows);
 };
 
 module.exports.getNewMessages = (roomId, start) => {
-  const queryStr = 'SELECT messages.*, users_name FROM messages INNER JOIN rooms ON messages.user = users.id WHERE messages.room = $1 AND messages.posted > $2';
+  const queryStr = 'SELECT messages.body, messages.room, messages.posted, users.name AS user FROM messages INNER JOIN users ON messages.user = users.id WHERE messages.room = $1 AND messages.posted > $2';
+  
   return pool.query(queryStr, [roomId, start])
     .then(({ rows }) => rows);
 };
 
 module.exports.getRooms = (uuid) => {
   const queryStr = 'SELECT rooms.* FROM users INNER JOIN users_rooms ON users.id = users_rooms.user INNER JOIN rooms ON users_rooms.room = rooms.id';
+  
   return pool.query(queryStr, [uuid])
     .then(({ rows }) => rows);
 };
 
 module.exports.createRoom = (uuid, roomName) => {
-  const findUserQuery = 'SELECT id as user FROM users WHERE uuid = $1';
+  const findUser = 'SELECT id as user FROM users WHERE uuid = $1';
   const createRoom = 'INSERT INTO rooms (name) VALUES ($1) RETURNING id as room';
   const linkUserToRoom = 'INSERT INTO users_rooms (user, room) VALUES ($1, $2)';
   
-  return pool.query(findUserQuery, [uuid])
+  return pool.query(findUser, [uuid])
     .then(({ rows }) => {
       const { id } = rows[0];
       return pool.query(createRoom, [roomName])
@@ -46,4 +50,17 @@ module.exports.createRoom = (uuid, roomName) => {
         })
         .then(room => pool.query(linkUserToRoom, [id, room]));
     });
+}
+
+module.exports.createUser = (userName, uuid) => {
+  const queryStr = 'INSERT INTO users (name, uuid) VALUES ($1, $2)';
+  
+  return pool.query(queryStr, [userName, uuid]); 
+}
+
+module.exports.findUser = (userName) => {
+  const queryStr = 'SELECT * FROM users WHERE name = $1';
+
+  return pool.query(queryStr, [userName])
+    .then(rows => rows);
 }
